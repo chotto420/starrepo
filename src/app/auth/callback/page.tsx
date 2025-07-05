614bb09cc715d449f1dceb5d49e637aca0cba366
chotto420 <kota.nishikawa@jarminal.com>
2025-07-05 17:33:51 +0900

    Fix callback auth method

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -5,7 +5,9 @@ import { supabase } from '@/lib/supabase'
 export default function Callback() {
   useEffect(() => {
     (async () => {
-      const { error } = await supabase.auth.getSessionFromUrl({ storeSession: true })
+      const {
+        error,
+      } = await supabase.auth.exchangeCodeForSession({ storeSession: true })
       if (error) {
         alert(`ログイン失敗: ${error.message}`)
         return
